58a7d3bbdbec774406a4ed72c931fe6fb057955d
unknown <akatkic@akatkicw350.corp.capgemini.com>
2016-11-04 16:23:06 +0100

    service added

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,18 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-app',
@@ -76,12 +64,27 @@ const HEROES: Hero[] = [
       margin-right: .8em;
       border-radius: 4px 0 0 4px;
     }
-  `]
+  `],
+  providers: [HeroService] //Create fresh instance of HeroService to be used by AppComponent
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  //Properties to expose for binding
   title = 'Tour of Heroes';
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void {
+    //.then is method of Promise
+    //heroes => this.heroes = heroes callback sets heroes property 
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
---
+++ app/hero.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+//TypeScript sees the @Injectable() decorator and emits metadata about our service, 
+//metadata that Angular may need to inject other dependencies into this service.
+@Injectable()
+export class HeroService {
+  //Note async call with promise.
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
+    }
+}
